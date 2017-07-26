e82070a1f36ee2eca57263d9ef893503a6066ef3
oscar-neiva <oscarneivaeneto@gmail.com>
2017-07-26 01:28:40 -0300

    lesson 16

--- models/ninja.js
+++ models/ninja.js
@@ -1,20 +1,32 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-//create ninja schema & model
-const NinjaSchema = new Schema({
-    name:{
+// create geolocation Schema
+const GeoSchema = new Schema({
+    type: {
         type: String,
-        required: [true, 'name field is required']
+        default: 'Point'
     },
-    rank:{
-        type: String 
-    },
-    available:{
-        type:Boolean,
-        default: false
+    coordinates: {
+        type: [Number],
+        index: '2dsphere'
     }
-    // add in geolocation
+});
+
+// create ninja Schema & model
+const NinjaSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Name field is required']
+    },
+    rank: {
+        type: String
+    },
+    available: {
+        type: Boolean,
+        default: false
+    },
+    geometry: GeoSchema
 });
 
 const Ninja = mongoose.model('ninja', NinjaSchema);
